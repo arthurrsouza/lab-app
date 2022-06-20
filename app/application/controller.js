7b5cf917d9874b1a0e0b45713ce03e466e8bf8f0
arthurrsouza <4565050+arthurrsouza@users.noreply.github.com>
2022-06-20 14:40:56 -0300

    Add ember-power-select on application controller

---
+++ app/application/controller.js
@@ -0,0 +1,9 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class ApplicationController extends Controller {
+    @tracked names = [
+        'Arthur', 'Sebastian', 'Patrick', 'Celia'
+    ]
+}
