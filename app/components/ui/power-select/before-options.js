76331a3ce1a07ad9607998860159c7b20b1f4c9e
arthurrsouza <4565050+arthurrsouza@users.noreply.github.com>
2022-06-20 14:42:15 -0300

    Customize options and before-options components

---
+++ app/components/ui/power-select/before-options.js
@@ -0,0 +1,22 @@
+// import PowerSelectBeforeOptionsComponent from 'ember-power-select/components/power-select/before-options'
+import PowerSelectBeforeOptionsComponent from '@glimmer/component';
+import { action } from '@ember/object'
+
+export default class UiPowerSelectBeforeOptionsComponent extends PowerSelectBeforeOptionsComponent {
+  
+  get allSelected() {
+    const { options, selected } = this.args.select;
+    return (selected && selected.length === options.length);
+  }
+
+  @action
+  toggleAll() {
+    const { options, selected } = this.args.select
+    if (this.allSelected) {
+      return this.args.select.actions.select(null);
+    }
+
+    this.args.select.actions.select(this.args.select.options);  
+  }
+
+}
